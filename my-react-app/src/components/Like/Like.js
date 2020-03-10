326f993600d9b7ac94bbea413702edfc20f154f6
Norman Calhoun <ndc1963@gmail.com>
2020-03-10 13:37:01 -0400

    like added

---
+++ my-react-app/src/components/Like/Like.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Like = props => {
+  return <div className="like__div">like button</div>;
+};
+
+export default Like;
---
+++ my-react-app/src/components/Like/Like.stories.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import "./Like.css";
+import Like from "./Like";
+
+//I am adding atributes we can access later
+storiesOf("Like", module).add("TEST", () => <Like />);
