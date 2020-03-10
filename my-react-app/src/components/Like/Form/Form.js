96247f91f27522798ae828d2adcaef53ce1fcd6e
Norman Calhoun <ndc1963@gmail.com>
2020-03-10 14:37:51 -0400

    added form class

---
+++ my-react-app/src/components/Like/Form/Form.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Form = props => {
+  return <div className="like__div">like button</div>;
+};
+
+export default Like;
---
+++ my-react-app/src/components/Like/Form/Form.stories.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import "./Form.css";
+import Like from "./Form";
+
+storiesOf("Form", module).add("Form", () => <Form />);
