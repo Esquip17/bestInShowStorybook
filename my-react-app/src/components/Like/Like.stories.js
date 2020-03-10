b079deccf7e6d3590d4c74c7e5602ac11016f3a1
Norman Calhoun <ndc1963@gmail.com>
2020-03-10 13:51:53 -0400

    like class added

--- my-react-app/src/components/Like/Like.stories.js
+++ my-react-app/src/components/Like/Like.stories.js
@@ -4,4 +4,6 @@ import "./Like.css";
 import Like from "./Like";
 
 //I am adding atributes we can access later
-storiesOf("Like", module).add("TEST", () => <Like />);
+storiesOf("Like", module)
+  .add("TEST", () => <Like />)
+  .add("Like_outline", () => <Like />);
